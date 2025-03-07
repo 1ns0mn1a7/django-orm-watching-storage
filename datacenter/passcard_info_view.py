e89980bc6d5e3e6f68ed0f415a354ab52a464333
1ns0mn1a7 <tyrischev.d.v@gmail.com>
2025-03-07 07:39:19 +0300

    Внес изменения после Review

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -3,6 +3,17 @@ from django.shortcuts import render, get_object_or_404
 from django.utils.timezone import localtime
 
 
+SECONDS_IN_HOUR = 3600
+SECONDS_IN_MINUTE = 60
+
+
+def format_duration(duration):
+    total_seconds = int(duration.total_seconds())
+    hours = total_seconds // SECONDS_IN_HOUR
+    minutes = (total_seconds % SECONDS_IN_HOUR) // SECONDS_IN_MINUTE
+    return f"{hours}ч {minutes}мин"
+
+
 def passcard_info_view(request, passcode):
     passcard = get_object_or_404(Passcard, passcode=passcode)
     visits = Visit.objects.filter(passcard=passcard)
@@ -12,7 +23,7 @@ def passcard_info_view(request, passcode):
             'entered_at': localtime(visit.entered_at).strftime(
                 "%d-%m-%Y %H:%M"
             ),
-            'duration': Visit.format_duration(visit.get_duration()),
+            'duration': format_duration(visit.get_duration()),
             'is_strange': visit.is_visit_long(minutes=60),
         }
         for visit in visits
