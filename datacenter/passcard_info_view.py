c9cb15ca1ccd4e54db59319b44bd241b4a84eb3f
1ns0mn1a7 <tyrischev.d.v@gmail.com>
2025-03-09 03:39:57 +0300

    Вывел формат времени в отдельный файл

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -1,17 +1,7 @@
 from datacenter.models import Passcard, Visit
 from django.shortcuts import render, get_object_or_404
 from django.utils.timezone import localtime
-
-
-SECONDS_IN_HOUR = 3600
-SECONDS_IN_MINUTE = 60
-
-
-def format_duration(duration):
-    total_seconds = int(duration.total_seconds())
-    hours = total_seconds // SECONDS_IN_HOUR
-    minutes = (total_seconds % SECONDS_IN_HOUR) // SECONDS_IN_MINUTE
-    return f"{hours}ч {minutes}мин"
+from datacenter.time_formatting import format_duration
 
 
 def passcard_info_view(request, passcode):
--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,17 +1,7 @@
 from django.shortcuts import render
 from django.utils.timezone import localtime
 from datacenter.models import Visit
-
-
-SECONDS_IN_HOUR = 3600
-SECONDS_IN_MINUTE = 60
-
-
-def format_duration(duration):
-    total_seconds = int(duration.total_seconds())
-    hours = total_seconds // SECONDS_IN_HOUR
-    minutes = (total_seconds % SECONDS_IN_HOUR) // SECONDS_IN_MINUTE
-    return f"{hours}ч {minutes}мин"
+from datacenter.time_formatting import format_duration
 
 
 def storage_information_view(request):
